1fd08e69ea7dd8ed3123915746d3aad837527003
mazhensai <xiao3333ma@gmail.com>
2019-08-05 18:58:07 +0800

    solved 557.Reverse_Words_in_a_String_III

---
+++ 557. Reverse Words in a String III.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(reverseWords("Let's take LeetCode contest"))
+}
+
+func reverseWords(s string) string {
+	strArr := strings.Split(s, " ")
+	for i, world := range strArr {
+		strArr[i] = revers(world)
+	}
+	return strings.Join(strArr, " ")
+}
+
+func revers(str string) string   {
+	bytes := []byte(str)
+	i, j := 0, len(bytes) - 1
+	for i < j {
+		bytes[i], bytes[j] = bytes[j], bytes[i]
+		i++
+		j--
+	}
+	return string(bytes)
+}

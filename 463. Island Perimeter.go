0c8af4fb19a7cc459e03b4af17b13c5d308bbceb
xiao333ma <xiao3333ma@gmail.com>
2019-11-05 22:43:25 +0800

    resolved 463. Island Perimeter

---
+++ 463. Island Perimeter.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := [][]int{
+		{0,1},
+
+	}
+
+	fmt.Println(islandPerimeter(a))
+}
+
+
+func islandPerimeter(grid [][]int) int {
+	sum := 0
+
+	row := len(grid)
+	col := len(grid[0])
+
+	for i := 0; i < row ; i++  {
+		for j := 0; j < col; j++  {
+			tem := 0
+
+			if grid[i][j] == 0 {
+				continue
+			}
+			// 上边
+			if i == 0 || grid[i-1][j] == 0 {
+				tem += 1
+			}
+
+			// 下边
+			if i == row - 1 || grid[i+1][j] == 0 {
+				tem += 1
+			}
+
+			// 左边
+
+			if j == 0 || grid[i][j - 1] == 0 {
+				tem += 1
+			}
+
+			// 右边
+
+			if j == col - 1 || grid[i][j + 1] ==0 {
+				tem += 1
+			}
+
+
+			sum += tem
+
+		}
+	}
+	return sum
+}

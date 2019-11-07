562cd9f7ffa07b59f81a0da0147091b517e3e43d
mazhensai <xiao3333ma@gmail.com>
2019-11-07 20:02:36 +0800

    Trim a Binary Search Tree

--- 669. Trim a Binary Search Tree.go
+++ 669. Trim a Binary Search Tree.go
@@ -1,6 +1,12 @@
 package main
 
-
+/*
+二叉搜索树的特点是，左节点比根节点小，右节点比根节点大
+对于这道题来说，由于要取 [L, R] 范围的树
+如果这个节点比 R 大，那么他的右节点更大，丢掉，去找他的左节点
+如果这个节点比 L 小，那么他的左节点更小，丢掉，去找他的右节点
+属于这个范围之间的，继续遍历即可
+*/
 func trimBST(root *TreeNode, L int, R int) *TreeNode {
 	if root == nil {
 		return root
